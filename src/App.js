048e5e31dcf9962b2c6c02b518caf24b20ff7be6
Timo Kuki <timo.kuki@studiogoldsmith.fi>
2024-12-05 22:25:48 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Header from './components/Header';
+import Header from './components/Header'
 import Footer from './components/Footer';
 import './App.css'; // Global styling if any
 import Dashboard from './components/Dashboard';
@@ -11,6 +11,7 @@ function App() {
       <main>
       <main>
         <Dashboard />
+        <h1>Mories</h1>
       </main>
       </main>
       <Footer />
